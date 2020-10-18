fb0f4c053f8d7517812a4c7da2ff36f7874106f0
Jericho Irvin <aldejerichoirvin@gmail.com>
2020-10-18 13:10:22 +0800

    Updates

---
+++ precache-manifest.58c4fa0e1ddb753fc024e71a70fb6506.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef35cd71111cd5c42f704f80eb3eab1a",
+    "url": "/budgeting-app/index.html"
+  },
+  {
+    "revision": "ddc7da0b5e44e320f14e",
+    "url": "/budgeting-app/static/js/2.3d563a9a.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/budgeting-app/static/js/2.3d563a9a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d686731a511ee5eaf375",
+    "url": "/budgeting-app/static/js/main.f3218c67.chunk.js"
+  },
+  {
+    "revision": "ce67079707edfab4b085",
+    "url": "/budgeting-app/static/js/runtime-main.6a063736.js"
+  }
+]);
